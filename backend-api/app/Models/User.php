3b6b79ae5c9800c9c537a730966ce65c9a7cbb5c
Sergej2011 <sergej.munic@gmail.com>
2025-09-28 15:54:31 +0200

    models

--- backend-api/app/Models/User.php
+++ backend-api/app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-
+ 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
